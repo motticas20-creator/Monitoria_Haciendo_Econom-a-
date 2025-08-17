7e56024896ae8bf5fcbe110127e474528c0c72ae
Maria Alejandra <motticas20@gmail.com>
2025-08-17 18:46:02 -0500

    Update Monitoria 1- Intro R.R
    Eliminar texto innecesario

--- Monitoria 1- Intro R.R
+++ Monitoria 1- Intro R.R
@@ -6,8 +6,6 @@
 ## Siempre limpiar el environment (memoria de R) antes de empezar a trabajar, 
 ## se hace con el codigo rm(list = ls())
 ## Definir el directorio antes de empezar a trabajar, se hace asi: setwd("el path a la carpeta")
-# Este es el script de la primera monitoria 
-# Esto es una modificación 
 
 ## Limpiar  enviroment
 rm(list = ls())
